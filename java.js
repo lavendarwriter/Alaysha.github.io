97c4dbd300f73f6fbe839362212292c753c80bfc
lavendarwriter <48302011+lavendarwriter@users.noreply.github.com>
2019-07-19 09:38:22 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+alert("Welcome To My Page!")
+
+function bigImg(x){
+x.style.height = "600px";
+x.style.width = "600px";
+
+}
+ function smallImg(x){
+x.style.height = "600px;"
+x.style.width = "600px";
+
+ }
